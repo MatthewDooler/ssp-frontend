9c691770224572652690e2f0da1ed0dcb5d20b01
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-01-31 08:58:38 +0000

    Live config

--- js/config.js
+++ js/config.js
@@ -1,3 +1,19 @@
+environment = get_environment();
+
 settings = {
-  apiEndpoint: 'http://127.0.0.1:5000'
+	environment: environment,
+	apiEndpoint: get_api_endpoint(environment)
+}
+
+function get_environment() {
+	var url = window.location.href
+	if (url.startsWith("file://")) return 'dev'
+	else return 'production'
+}
+
+function get_api_endpoint(environment) {
+	switch (environment) {
+		case 'dev': return 'http://127.0.0.1:5000'
+		case 'production': return 'https://ssp-api-v2.herokuapp.com'
+	}
 }
